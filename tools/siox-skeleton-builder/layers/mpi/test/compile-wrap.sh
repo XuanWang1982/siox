51ee89ff9a08b4463368c614ad7868f7b7bfca67
XuanWang1982 <XuanWang1982@users.noreply.github.com>
2014-06-02 18:45:17 +0200

    MPI Wrapper test for writing gpfs file

--- tools/siox-skeleton-builder/layers/mpi/test/compile-wrap.sh
+++ tools/siox-skeleton-builder/layers/mpi/test/compile-wrap.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 OUTPUT_FILE=mpi-wrapper.c
+alias sioxmpicc="mpicc -L/home/siox-devel/install-xuan/siox/lib64 -lsiox-monitoring-siox-ll -lboost_regex -L/home/siox-devel/install-tools/boost_1_53_0/lib -lboost_system -lboost_serialization -lboost_thread -lboost_program_options -L/home/siox-devel/install-tools/gcc-4.7.3/lib64 -L/homesiox-devel/install-tools/gcc-4.7.3/lib -lstdc++"
+
+alias sioxmpirun="SIOX_CONFIGURATION_PROVIDER_ENTRY_POINT=/home/sioxaagu/siox/src/monitoring/low-level-c/test/siox.conf mpirun --mca io ompio"
 
 echo 'Generating' $OUTPUT_FILE
 sleep 2

80930d98a5b68fbbb3e964a91b7fdc844b88f522
XuanWang1982 <XuanWang1982@users.noreply.github.com>
2014-06-02 13:48:29 +0200

    compile-wrap.sh for MPI wrapper

--- tools/siox-skeleton-builder/layers/mpi/compile-wrap.sh
+++ tools/siox-skeleton-builder/layers/mpi/compile-wrap.sh
@@ -4,7 +4,7 @@ OUTPUT_FILE=mpi-wrapper.c
 
 python ../../siox-skeleton-builder.py -s wrap -t ../../template.py -o $OUTPUT_FILE mpi-wrapper.h > wrap-options.txt
 
-mpicc $OUTPUT_FILE -c -fPIC -I ../../../low-level-C-interface `pkg-config --cflags glib-2.0`
+mpicc $OUTPUT_FILE -c -fPIC -std=c99 -I ../../../low-level-C-interface `pkg-config --cflags glib-2.0`
 #gcc $OUTPUT_FILE -c -fPIC -I ../../../low-level-C-interface `pkg-config --cflags glib-2.0`
 #gcc ${OUTPUT_FILE%%.c}.o -o libsiox-posix-ll-wrapper.so -shared -ldl `pkg-config --libs glib-2.0`
 

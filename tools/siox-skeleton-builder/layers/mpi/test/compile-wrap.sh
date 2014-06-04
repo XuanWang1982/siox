4a96011a36612acf1b4144b6027c13f7200cbb0f
XuanWang1982 <XuanWang1982@users.noreply.github.com>
2014-06-04 16:20:21 +0200

    Debug enabled

--- tools/siox-skeleton-builder/layers/mpi/test/compile-wrap.sh
+++ tools/siox-skeleton-builder/layers/mpi/test/compile-wrap.sh
@@ -14,12 +14,12 @@ python ../../../siox-skeleton-builder.py -s wrap -t ../../../template.py -o $OUT
 
 echo 'Compiling' $OUTPUT_FILE
 sleep 1
-sioxmpicc $OUTPUT_FILE -c -fPIC -std=c99 -I ../../../low-level-C-interface `pkg-config --cflags glib-2.0`
+sioxmpicc $OUTPUT_FILE -g -c -fPIC -std=c99 -I ../../../low-level-C-interface `pkg-config --cflags glib-2.0`
 sioxmpicc write_gpfs_file.c -c
 
 echo 'Appending the output of the wrapper to compile the program'
 sleep 1
-sioxmpicc write_gpfs_file.o mpi-wrapper.o -o write_gpfs_file_wrapper $WRAP_MPI_FUNCTIONS
+sioxmpicc write_gpfs_file.o mpi-wrapper.o -g -o write_gpfs_file_wrapper $WRAP_MPI_FUNCTIONS
 
 echo 'Running wrapped code'
 sioxmpirun -np 1 write_gpfs_file_wrapper

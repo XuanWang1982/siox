3ffea486f87e50fb785b1614fac5a517d35cf1ca
XuanWang1982 <XuanWang1982@users.noreply.github.com>
2014-06-25 16:50:37 +0200

    Development of Optimizer Plugin for SIOX

--- src/knowledge/optimizer/modules/OptimizerStandardImplementation.cpp
+++ src/knowledge/optimizer/modules/OptimizerStandardImplementation.cpp
@@ -69,7 +69,7 @@ namespace knowledge {
 				bool flag = false;
 				flag = isPluginRegistered(aid);
 cout << "Is the optimizer plugin registered?" << boolalpha << flag << endl;
-registerPlugin(aid, expert);
+registerPlugin(aid);
 
 				if(!flag){
 					cout << "TODO registerPlugin()" << endl;

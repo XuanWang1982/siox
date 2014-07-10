8affc34e32f374282a49932c2054af0a91054470
XuanWang1982 <XuanWang1982@users.noreply.github.com>
2014-07-10 16:49:08 +0200

    Development of Optimizer Plugin for SIOX

--- src/knowledge/optimizer/modules/OptimizerOptions.hpp
+++ src/knowledge/optimizer/modules/OptimizerOptions.hpp
@@ -1,7 +1,19 @@
 #include <core/component/component-options.hpp>
+#include <knowledge/optimizer/OptimizerPluginOptions.hpp>
+#include <monitoring/activity_multiplexer/ActivityMultiplexerPluginOptions.hpp>
+
+#include <vector>
+#include <utility>
 
 //@serializable
-class OptimizerOptions : public core::ComponentOptions{
+class OptimizerOptions : public ActivityMultiplexerPluginOptions{
+	ComponentReference optimizer;
+
+	// the layer we are operating on must be the layer we are connected to.
+	string interface;
+	string implementation;
+
+	vector<pair<string,string> > hintAttributes;	// attributes containing hints, e.g. ('MPI','hint/noncollReadBuffSize')
 
 };
 
